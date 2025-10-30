0898e0877f6f742c686efce2ee8e0a566bd4a30d
Impulse <ftcimpulse1566@gmail.com>
2025-10-30 12:34:08 +0300

    version 1.4.5.2 feature drop 30.10

--- opmodes/Auto.java
+++ opmodes/Auto.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Camera;
@@ -31,7 +32,6 @@ public class Auto extends LinearOpMode {
                 gamepad2, imu, null, cam, wheel, this);
 
         wheel.reset_encoders();
-
         waitForStart();
 
         r.move_xy(x, x1, y, y1, 0,0.012);
